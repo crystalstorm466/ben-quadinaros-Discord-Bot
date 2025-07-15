511090ae703d5fdee77ac5488696a99194fe1e75
David Holland <DcHolland_89@msn.com>
2025-07-14 22:58:19 -0400

    Added memory for the counter by saving counter to counter.txt :

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,50 @@
 
 static std::string BOT_TOKEN = std::getenv("SPAM_BOT_TOKEN");
 const dpp::snowflake BEN_CHANNEL_ID = 1378103577196761238;
+
+int readCounter() {
+  std::ifstream readCounter("counter.txt");
+  std::string line;
+
+  if (readCounter.is_open() && std::getline(readCounter, line)) {
+    try {
+      return std::stoi(line);
+    } catch (std::invalid_argument& e) {
+      std::cerr << "Invalid counter in counter.txt" << std::endl;
+      return 0;
+    } catch (const std::out_of_range& e) {
+      std::cerr << "Counter out of range" << std::endl;
+      return 0;
+    }
+  }
+}
+
+void writeCounter(int counter) {
+  std::ofstream outFile("counter.txt");
+  if (outFile.is_open()) {
+    outFile << counter << std::endl;
+    outFile.close();
+    return;
+  } else {
+    std::cerr << "Could not open counter.txt for writing" << std::endl;
+    return;
+  }
+}
 void on_ready_handler(dpp::cluster &bot, const dpp::ready_t &event) {
     // This function is called when the bot is ready
     std::cout << "Bot is ready! Logged in as " << bot.me.username << std::endl;
-
+        int counter = readCounter();
     // Optionally, you can send a message to a specific channel
     dpp::message msg (BEN_CHANNEL_ID, "");
     msg.add_file("ben.jpg", dpp::utility::read_file("ben.jpg"));
-    int counter;
+    
     while(true) {
-             
+      
       bot.message_create(msg);
+       
+    
       counter++;
+      writeCounter(counter);
       if (counter % 10 == 0) {
         dpp::message countermsg (BEN_CHANNEL_ID, "Sent " + std::to_string(counter) + " times.");
         std::cout << "Sent ben quadinaros " << counter << " times. \n" << std::endl;
@@ -42,7 +74,6 @@ int main() {
         std::cerr << "Error: BOT_TOKEN environment variable is not set." << std::endl;
         return EXIT_FAILURE;
     }
-
   dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
   bot.on_log(dpp::utility::cout_logger());
 
